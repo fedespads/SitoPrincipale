a146826507dc70058c9a8150fabd3d59867c52d7
fedespads <134450966+fedespads@users.noreply.github.com>
2023-10-13 16:59:33 +0200

    Delete Classifica directory

--- Classifica/create/index.js
+++
@@ -1,7 +0,0 @@
-let nuovotitolo = document.querySelector('.nuovotitolo')
-nuovotitolo.addEventListener('keypress',e=>{
-  if(e.key=='Enter'&&nuovotitolo.value!=""){
-    localStorage.setItem('cdm',nuovotitolo.value);
-    window.location.href = '../build';
-  }
-})
