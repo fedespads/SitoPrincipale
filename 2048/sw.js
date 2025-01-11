6177cb82273a39506f0af1643dbb13382c577944
fedespads <134450966+fedespads@users.noreply.github.com>
2025-01-11 22:41:51 +0100

    Add files via upload

---
+++ 2048/sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'game-2048-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/index.js',
+  '/manifest.json',
+  'https://fedespada.com/font/SF-Pro-Display-Bold.ttf',
+  'https://fedespada.com/font/Bebas.ttf',
+  'https://code.jquery.com/jquery-3.6.0.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
