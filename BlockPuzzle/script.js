aea297a518c6e5a4a4f169695754d96039ed61d2
fedespads <134450966+fedespads@users.noreply.github.com>
2025-01-16 20:24:55 +0100

    Add files via upload

--- BlockPuzzle/script.js
+++ BlockPuzzle/script.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/BlockPuzzle/sw.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+
 function script() {
   document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector(".grid");
