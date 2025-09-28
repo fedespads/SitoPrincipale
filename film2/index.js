134c4eb78e613713681ab28c9babd74a0017a0ea
fedespads <134450966+fedespads@users.noreply.github.com>
2025-09-28 03:51:45 +0200

    Delete film2 directory

--- film2/index.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('input').addEventListener('keypress',e=>{
-  if(e.key=='Enter'){
-    window.location.href = 'https://cb01.financial/?s='+document.querySelector('input').value
-  }
-})
